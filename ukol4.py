2aef889682e16243de9e411cf11b5dcfb3813f28
AnyKaf <anezka@zzaba.cz>
2023-02-04 11:23:46 +0800

    ukoliky

---
+++ ukol4.py
@@ -0,0 +1,25 @@
+def format_cisla(cislo):
+    if len(cislo) == 9:
+        print("True")
+    elif len(cislo) == 13:
+        predvolba = "+420"
+        if cislo[0:4] == predvolba:
+            print("True")
+        else:
+            print("False")
+            quit()
+    else:
+        print("False")
+        quit()
+
+def cena_zpravy(zprava):
+    cena = ((len(zprava) // 180) + 1) * 3
+    return cena
+
+
+cislo_zadano = input("Zadejte telefonní číslo: ")
+format_cisla(cislo_zadano)
+
+zprava_zadano = input("Zadejte text vaší SMS: ")
+cena_final = cena_zpravy(zprava_zadano)
+print(f"Cena vaší SMS je {cena_final} Kč")
